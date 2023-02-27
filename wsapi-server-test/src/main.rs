c6a3195cc3f13e0955d33796a53925d11b9434b8
Natalia Chernysheva <Natalia.Chernysheva@centric.eu>
2023-02-27 12:17:28 +0100

    wsapi server work with config

--- wsapi-server-test/src/main.rs
+++ wsapi-server-test/src/main.rs
@@ -1,20 +1,43 @@
-use actix_web::{get, web, post, App, HttpResponse, HttpServer, Responder, Result};
-use serde::Serialize;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result, get, post};
+use serde::{Serialize, Deserialize};
+use std::fs::File;
 
 #[derive(Serialize)]
 struct MyObj {
+    service: String,
     name: String,
 }
 
-#[get("/label/{name}")]
-async fn get_label(name: web::Path<String>) -> Result<impl Responder> {
+#[derive(Deserialize, Debug)]
+struct Route {
+    url: String,
+    service: String,
+    method: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Config{
+    host: String,
+    port: u16,
+    routes: Vec<Route>,
+}
+
+async fn get_postlabel(name: web::Path<String>) -> Result<impl Responder> {
     let obj = MyObj {
+        service: "postlabel".to_string(),
         name: name.to_string(),
     };
     Ok(web::Json(obj))
 }
 
-#[post("/label")]
+async fn get_dhllabel(name: web::Path<String>) -> Result<impl Responder> {
+    let obj = MyObj {
+        service: "dhllabel".to_string(),
+        name: name.to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
 async fn create_label(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
@@ -22,12 +45,19 @@ async fn create_label(req_body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let config_open_result = File::open("config.yml").expect("Could not open file.");
+    let config: Config = serde_yaml::from_reader(config_open_result).unwrap();
+
     HttpServer::new(|| {
         App::new()
-            .service(get_label)
-            .service(create_label)
+            
+            .service(web::scope("/label")
+                .route("/post/{name}", web::get().to(get_postlabel))
+                .route("/dhl/{name}", web::get().to(get_dhllabel))
+                .route("", web::post().to(create_label))
+            )
     })
-    .bind(("127.0.0.1", 8081))?
+    .bind((config.host, config.port))?
     .run()
     .await
 }
