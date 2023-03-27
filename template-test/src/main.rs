008ba352663d32f0f0dba562a5d87021a342c83b
Natalia Chernysheva <Natalia.Chernysheva@centric.eu>
2023-03-27 14:41:57 +0200

    Add a condition that field is not denifed

--- template-test/src/main.rs
+++ template-test/src/main.rs
@@ -48,6 +48,13 @@ fn use_template_address(address: MyAddress) -> Result<String, tera::Error> {
     Ok(rendered)
 }
 
+fn use_template_businesscard(businesscard: serde_json::Value) -> Result<String, tera::Error> {
+    let mut tera = Tera::default();
+    tera.add_template_file(Path::new("businesscard.json"), None)?;
+    let rendered = tera.render("businesscard.json", &Context::from_serialize(businesscard)?)?;
+    Ok(rendered)
+}
+
 fn main() -> tera::Result<()> {
     let my_page = MyPage{my_title: String::from("My Page"), 
                          my_body: String::from("Welcome to my page!")};
@@ -61,5 +68,26 @@ fn main() -> tera::Result<()> {
     println!("{}", use_template_page(my_page)?);
     println!("{}", json_address(my_address.clone()));
     println!("{}", use_template_address(my_address)?);
+
+    // use template for empty field
+    println!("");
+    println!("Tests with empty field");
+    let his_businesscard = serde_json::json!({
+        "name": String::from("John Joy"),
+        "phone": String::from("+0123456789"),
+        "email": String::from("johnjoy@example.com"),
+        "fax": String::from("+0123456788")
+    });
+
+    let her_businesscard = serde_json::json!({
+        "name": String::from("Taja Joy"),
+        "phone": String::from("+0123456777"),
+        "email": String::from("tajajoy@example.com"),
+    });
+
+    println!("Test with fax field");
+    println!("{}", use_template_businesscard(his_businesscard)?);
+    println!("Test without fax field");
+    println!("{}", use_template_businesscard(her_businesscard)?);
     Ok(())
 }
