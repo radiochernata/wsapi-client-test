915a60c191e44cead56dbac56c96596738f9a18b
Natalia Chernysheva <Natalia.Chernysheva@centric.eu>
2023-05-15 15:14:18 +0200

    Rewrite templates to use vector

--- template-test/src/main.rs
+++ template-test/src/main.rs
@@ -1,6 +1,6 @@
 use tera::{Tera, Context};
 use std::path::Path;
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize, Serializer, ser::SerializeStruct};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MyPage
@@ -9,7 +9,7 @@ struct MyPage
     my_body: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone)]
 struct MyAddress
 {
     my_first_name: String,
@@ -20,6 +20,21 @@ struct MyAddress
     my_post_code: String,
 }
 
+impl Serialize for MyAddress {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let name = format!("{} {}", self.my_first_name, self.my_second_name);
+        let address = format!("{} {}, {} {}", self.my_post_code, self.my_city, self.my_street, self.my_house);
+
+        let mut obj = serializer.serialize_struct("MyAddress", 2)?;
+        obj.serialize_field("name", &name)?;
+        obj.serialize_field("address", &address)?;
+        obj.end()
+    }
+}
+
 fn json_page(page: MyPage) -> serde_json::Value {
     serde_json::json!({
         "title": page.my_title,
@@ -27,10 +42,11 @@ fn json_page(page: MyPage) -> serde_json::Value {
     })
 }
 
-fn json_address(address: MyAddress) -> serde_json::Value {
+fn json_address(address: Vec<MyAddress>) -> serde_json::Value {
+    let json_obj = serde_json::to_value(address).unwrap();
     serde_json::json!({
-        "name": format!("{} {}", address.my_first_name, address.my_second_name),
-        "address": format!("{} {}, {} {}", address.my_post_code, address.my_city, address.my_street, address.my_house)
+        "label": "My address book",
+        "addresses": json_obj
     })
 }
 
@@ -41,7 +57,7 @@ fn use_template_page(page: MyPage) -> Result<String, tera::Error> {
     Ok(rendered)
 }
 
-fn use_template_address(address: MyAddress) -> Result<String, tera::Error> {
+fn use_template_address(address: Vec<MyAddress>) -> Result<String, tera::Error> {
     let mut tera = Tera::default();
     tera.add_template_file(Path::new("address.json"), None)?;
     let rendered = tera.render("address.json", &Context::from_serialize(json_address(address))?)?;
@@ -56,18 +72,28 @@ fn use_template_businesscard(businesscard: serde_json::Value) -> Result<String, 
 }
 
 fn main() -> tera::Result<()> {
-    let my_page = MyPage{my_title: String::from("My Page"), 
-                         my_body: String::from("Welcome to my page!")};
-    let my_address = MyAddress{my_first_name: String::from("Sherlock"),
-                               my_second_name: String::from("Holmes"),
-                               my_city: String::from("London"),
-                               my_street: String::from("Baker street"),
-                               my_house: String::from("221B"),
-                               my_post_code: String::from("NW1 6XE")};
+    let my_page: MyPage = MyPage{my_title: String::from("My Page"), 
+                            my_body: String::from("Welcome to my page!")};
+    let address_brother: MyAddress = MyAddress{my_first_name: String::from("Sherlock"),
+                                my_second_name: String::from("Holmes"),
+                                my_city: String::from("London"),
+                                my_street: String::from("Baker street"),
+                                my_house: String::from("221B"),
+                                my_post_code: String::from("NW1 6XE")};
+    let address_sister: MyAddress = MyAddress{my_first_name: String::from("Sharlin"),
+                                my_second_name: String::from("Holmes"),
+                                my_city: String::from("London"),
+                                my_street: String::from("Baker street"),
+                                my_house: String::from("221B"),
+                                my_post_code: String::from("NW1 6XE")};
+    
+    let mut my_address_book: Vec<MyAddress> = Vec::new();
+    my_address_book.push(address_brother);
+    my_address_book.push(address_sister);
 
     println!("{}", use_template_page(my_page)?);
-    println!("{}", json_address(my_address.clone()));
-    println!("{}", use_template_address(my_address)?);
+    println!("{}", json_address(my_address_book.clone()));
+    println!("{}", use_template_address(my_address_book)?);
 
     // use template for empty field
     println!("");
